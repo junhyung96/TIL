9c96c899ee25b5de123433f23f20abfbdb5f337f
junhyung <wnsgud0895@gmail.com>
2024-01-18 18:00:40 +0900

    1

---
+++ 알고리즘 문제 풀이/BOJ/BOJ_Java/src/Main1009.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main1009 {
+    public static void main(String[] args) throws IOException {
+        Bufferedreader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i=0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readline());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            
+
+
+        }
+    }
+}
