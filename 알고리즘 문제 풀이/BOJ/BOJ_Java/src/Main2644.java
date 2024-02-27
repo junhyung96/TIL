37574dc6ef539dd4a45b9abfb93e621685005cf7
junhyung <wnsgud0895@gmail.com>
2024-02-27 23:52:13 +0900

    1

---
+++ 알고리즘 문제 풀이/BOJ/BOJ_Java/src/Main2644.java
@@ -0,0 +1,13 @@
+import javax.swing.plaf.synth.SynthTextAreaUI;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main2644 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+
+    }
+}
