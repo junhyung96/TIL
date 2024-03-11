da9a1aa1dd60980992c0811a4fe48fe00c097b4b
junhyung <wnsgud0895@gmail.com>
2024-03-11 22:01:02 +0900

    1

--- 알고리즘 문제 풀이/BOJ/BOJ_Java/src/Main1874.java
+++ 알고리즘 문제 풀이/BOJ/BOJ_Java/src/Main1874.java
@@ -3,11 +3,31 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main1874 {
+    // 같은 정수가 두번 나오는 일은 없다.
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-        for (int i=0; i<n; i++){
-
-        }
+//        int curNum = 1;
+        int[] stack = new int[n];
+        int top = -1;
+        int nxt = Integer.parseInt(br.readLine());
+        int i = 0;
+        int cnt = 0;
+        while (true) {
+            // 종료 조건 더 이상 입력할게 없을 때
+            if (top != -1 && stack[top] == nxt) {
+                nxt = Integer.parseInt(br.readLine());
+                sb.append("-\n");
+                top -= 1;
+                continue;
+            }
+            top += 1;
+            i += 1;
+            stack[top] = i+1;
+            sb.append("+\n");
+            }
+        if (top == -1) System.out.println(sb);
+        else System.out.println("NO");
     }
 }
--- 알고리즘 문제 풀이/BOJ/BOJ_Java/src/Main1920BinarySearch.java
+++ 알고리즘 문제 풀이/BOJ/BOJ_Java/src/Main1920BinarySearch.java
@@ -1,35 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 // 특정 값 조회
-// 해시맵 생각보다 느림
+// 이진 탐색
 public class Main1920BinarySearch {
-    static int[] isNumber;
+    static int[] nums;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
-        HashMap<Integer, Boolean> hashMap = new HashMap<>();
-
-//        isNumber = new boolean[100_100];
         StringTokenizer st = new StringTokenizer(br.readLine());
+        nums = new int[N];
         for (int i=0; i<N; i++){
-            int num = Integer.parseInt(st.nextToken());
-            hashMap.put(num, true);
-//            isNumber[num] = true;
+            nums[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(nums);
         int M = Integer.parseInt(br.readLine());
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
         for (int i=0; i<M; i++){
-            int num = Integer.parseInt(st2.nextToken());
-            if (hashMap.containsKey(num)) sb.append("1\n");
-//            if (isNumber[num]) System.out.println(1);
+            if (binarySearch(Integer.parseInt(st.nextToken()))) sb.append("1\n");
             else sb.append("0\n");
         }
         System.out.println(sb);
+    }
 
+    static boolean binarySearch(int x) {
+        int start = 0;
+        int end = nums.length - 1;
+        int mid = (start + end) / 2;
+
+        while (start <= end) {
+            if (x < nums[mid]) end = mid-1;
+            else if (x > nums[mid]) start = mid+1;
+            else return true; // x 를 찾으면 true 반환
+            mid = (start + end) / 2;
+        }
+        // 못 찾으면 false 반환
+        return false;
     }
 }
