2b35ed967ab76200a619123eadef839e191c3f88
junhyung <wnsgud0895@gmail.com>
2024-03-19 17:49:07 +0900

    1

--- 알고리즘 문제 풀이/BOJ/스티커.py
+++ 알고리즘 문제 풀이/BOJ/스티커.py
@@ -1,20 +1,43 @@
-# import sys
-# input = sys.stdin.readline
+import sys
+input = sys.stdin.readline
 
-# T = int(input())
+T = int(input())
 
-# def get_max_sum(length, graph):
-#     dp = [list([0, 0] for _ in range(length)) for _ in range(2)]    # 해당 좌표 스티커를 안뗐으면 인덱스0  뗐으면1
-#     dp[0][0][1] = graph[0][0]
-#     dp[0][1][0] = max(graph[0][0], graph[0][1])
-#     dp[0][1][1] = graph[0][1]
-#     for i in range(2, n):
+def get_max_sum(length, graph):
+    dp = [list([0, 0] for _ in range(length)) for _ in range(2)]    # 해당 좌표 스티커를 안뗐으면 인덱스0  뗐으면1
+    dp[0][0][1] = graph[0][0]
+    dp[1][0][1] = graph[1][0]
 
+    dp[0][1][0] = max(graph[0][0], graph[1][0])
+    dp[0][1][1] = max(dp[0][0][0] + graph[0][1], dp[1][0][1] + graph[0][1])
 
-# for test_case in range(T):
-#     n = int(input())
-#     stickers = [list(map(int, input().split())) for _ in range(2)]
-#     print(get_max_sum(n, stickers))
+    
+    dp[1][1][0] = max(dp[0][0][1], dp[0][1][0], dp[1][0][0], dp[0][1][1], dp[1][0][1])
+    dp[1][1][1] = max(dp[1][0][0] + graph[1][1], dp[0][0][1] + graph[1][1])
+
+    for i in range(2, n):
+        # 해당 좌표 스티커를 안뗐을 경우
+        dp[0][i][0] = max(dp[0][i-1][0], dp[0][i-1][1], 
+                          dp[0][i-2][0], dp[0][i-2][1], 
+                          dp[1][i-1][0], dp[1][i-1][1])
+        dp[1][i][0] = max(dp[0][i-1][0], dp[0][i-1][1], 
+                          dp[0][i][0], dp[0][i][1], dp[1][i-1][0], dp[1][i-1][1], dp[1][i-2][0], dp[1][i-2][1])
+        # 해당 좌표 스티커를 뗐을 경우
+        dp[0][i][1] = max(dp[0][i-1][0] + graph[0][i], 
+                          dp[0][i-2][0] + graph[0][i], dp[0][i-2][1] + graph[0][i],
+                          dp[1][i-1][0] + graph[0][i], dp[1][i-1][1] + graph[0][i])
+        dp[1][i][1] = max(dp[0][i-1][0] + graph[1][i], dp[0][i-1][1] + graph[1][i], 
+                          dp[1][i-2][0] + graph[1][i], dp[1][i-2][1] + graph[1][i])
+    # print(dp)
+    return(max(dp[1][n-1][0], dp[1][n-1][1], dp[0][n-1][0], dp[0][n-1][1]))
+
+for test_case in range(T):
+    n = int(input())
+    stickers = [list(map(int, input().split())) for _ in range(2)]
+    if n == 1:
+        print(max(stickers[0][0], stickers[1][0]))
+    else:
+        print(get_max_sum(n, stickers))
     
 
 
